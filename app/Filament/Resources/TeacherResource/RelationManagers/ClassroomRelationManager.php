e553f2f479e880b4fb8a2b51018abad0536f53ca
Dimas Ahmad Rizal <dimasahmadrizal3@gmail.com>
2023-09-10 22:11:21 +0700

    create option form

--- app/Filament/Resources/TeacherResource/RelationManagers/ClassroomRelationManager.php
+++ app/Filament/Resources/TeacherResource/RelationManagers/ClassroomRelationManager.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Resources\TeacherResource\RelationManagers;
 
+use App\Models\Classroom;
 use Filament\Forms;
+use Filament\Forms\Components\Actions\Action;
+use Filament\Forms\Components\Hidden;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Form;
+use Filament\Forms\Set;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Columns\ToggleColumn;
@@ -22,11 +26,31 @@ class ClassroomRelationManager extends RelationManager
             ->schema([
                 Select::make("classroom_id")
                     ->label("Kelas")
-                    ->options(\App\Models\Classroom::all()->pluck('name', 'id'))
+                    ->relationship("classroom", "name")
+                    ->createOptionForm([
+                        Forms\Components\TextInput::make('name')
+                            ->required()
+                            ->maxLength(255)->unique(Classroom::class, 'name')
+                            ->live()
+                            ->afterStateUpdated(fn (Set $set, ?string $state) => $set('slug', \Str::slug($state))),
+                        Hidden::make('slug'),
+                    ])
+                    ->createOptionAction(
+                        fn (Action $action) => $action->modalHeading("Add Classroom")->modalButton("Add Classroom")->modalWidth('2xl'),
+                    )
                     ->searchable(),
                 Select::make("period_id")
                     ->label("Tahun Ajaran")
                     ->options(\App\Models\Period::all()->pluck('name', 'id'))
+                    ->relationship("period", "name")
+                    ->createOptionForm([
+                        Forms\Components\TextInput::make('name')
+                            ->required()
+                            ->maxLength(255)->unique(Classroom::class, 'name')
+                    ])
+                    ->createOptionAction(
+                        fn (Action $action) => $action->modalHeading("Add Period")->modalButton("Add Period")->modalWidth('2xl'),
+                    )
                     ->searchable(),
             ]);
     }
