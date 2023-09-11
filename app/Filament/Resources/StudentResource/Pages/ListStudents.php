09ce2a4502eecf786737279a36570f64e69cd44e
Dimas Ahmad Rizal <dimasahmadrizal3@gmail.com>
2023-09-11 09:37:19 +0700

    import excel

--- app/Filament/Resources/StudentResource/Pages/ListStudents.php
+++ app/Filament/Resources/StudentResource/Pages/ListStudents.php
@@ -3,8 +3,12 @@
 namespace App\Filament\Resources\StudentResource\Pages;
 
 use App\Filament\Resources\StudentResource;
+use App\Imports\ImportStudents;
+use App\Models\Student;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Contracts\View\View;
+use Excel;
 
 class ListStudents extends ListRecords
 {
@@ -15,5 +19,19 @@ class ListStudents extends ListRecords
         return [
             Actions\CreateAction::make(),
         ];
+    }
+
+    function getHeader(): ?View
+    {
+        $data = Actions\CreateAction::make();
+        return view('filament.resources.studentResource.upload-file-header', compact('data'));
+    }
+
+    public $file = '';
+
+    function save() : void {
+        if($this->file) {
+            Excel::import(new ImportStudents, $this->file);
+        }
     }
 }
